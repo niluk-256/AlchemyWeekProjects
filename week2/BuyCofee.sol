03db342edfedbbb1528632a0a75be51cb10e4091
Niluk <67406138+niluk-256@users.noreply.github.com>
2022-08-13 09:13:22 +0530

    Update BuyCofee.sol

--- week2/BuyCofee.sol
+++ week2/BuyCofee.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+
 contract BuyCofee {
     event OwnershipTransferred(
         address indexed previousOwner,
@@ -30,7 +31,7 @@ contract BuyCofee {
         require(msg.sender == owner);
         _;
     }
-//---------------------
+//---------------------------
     function transferOwnership(address newOwner) public isOwner {
         require(newOwner != address(0));
         // require(newOwner != owner);
@@ -40,7 +41,7 @@ contract BuyCofee {
     function whoIsOwner()public view returns (address){
         return owner;
     }
- //------------------------------
+    //------------------------------------------------
     function getMemos() public view returns (Memo[] memory) {
         return memos;
     }
@@ -54,9 +55,21 @@ contract BuyCofee {
         memos.push(Memo(msg.sender, block.timestamp, _name, _message));
         emit NewMemo(msg.sender, block.timestamp, _name, _message);
     }
+    //------------------------...
+uint costSpecialCofee = 0.00299 ether;
+ function buyCofee2(string memory _name, string memory _message)
+        public
+        payable
+    {
+        require(msg.value > costSpecialCofee, "You have to pay more ETH ");
+
+        memos.push(Memo(msg.sender, block.timestamp, _name, _message));
+        emit NewMemo(msg.sender, block.timestamp, _name, _message);
+    }
+//--------------------------------------------------------
+
 
     function withdrawTips() public {
         require(owner.send(address(this).balance));
     }
 }
-
